492f66dbfc57b0e25d3ccd7e18e376022ea1e992
frbar <felipe.barbosa@gmail.com>
2023-04-19 12:00:14 -0300

    lets see if it works

---
+++ index.js
@@ -0,0 +1,26 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+const { Configuration, OpenAIApi } = require('openai');
+const readline = require('readline');
+
+const openAi = new OpenAIApi(
+    new Configuration({
+        apiKey: process.env.OPENAI_API_KEY,
+    })
+);
+
+const userInterface = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+userInterface.prompt();
+userInterface.on('line', async (input) => {
+    const response = await openAi.createChatCompletion({
+        model: 'gpt-3.5-turbo',
+        messages: [{ role: 'user', content: input }],
+    });
+    console.log(response.data.choices[0].message.content);
+    userInterface.prompt();
+});
